9160474a18ac3c93df82d275a6819673d1494479
Roberti Pichkhnarashvili <robaflex77@gmail.com>
2026-09-05 15:10:41 +0400

    Added category model

---
+++ models/category.py
@@ -0,0 +1,11 @@
+from datetime import datetime, timezone 
+from database.database import Base
+from sqlalchemy import Column, ForeignKey, String, Integer, DateTime
+
+class Category(Base):
+  __tablename__ = "categories"
+
+  id = Column(Integer, primary_key=True, index=True)
+  name = Column(String(50), nullable=False)
+  user_id = Column(Integer, ForeignKey("users.id"), nullable=True)
+  created_at = Column(DateTime, default=lambda: datetime.now(timezone.utc) )
