fdbb54ddb081f62443a436bf90bb5d3ddb6cf025
Roberti Pichkhnarashvili <robaflex77@gmail.com>
2026-09-09 19:35:23 +0400

    added a get_current_user defenition which returns the current user_

--- security/jwt.py
+++ security/jwt.py
@@ -1,10 +1,22 @@
 from datetime import datetime, timezone, timedelta
+from typing import Annotated
+
+from fastapi import Depends, HTTPException
+from fastapi.security import OAuth2PasswordBearer
+
 from jose import JWTError, jwt
+
 from pydantic import BaseModel
+from models.user import User
+
+from routers.auth import db_dependency
+
 
 JWT_SECRET_KEY = "8331cde2c4c9fb46b17e12e4350016fc919ae50556e28e1d77d565939c84f602"
 JWT_ALGORITHM = "HS256"
 ACCESS_TOKEN_EXPIRE_MINUTES = 100
+
+oauth2_scheme = OAuth2PasswordBearer(tokenUrl="/auth/login")
 
 class TokenResponse(BaseModel):
   access_token: str 
@@ -41,3 +53,34 @@ def decode_access_token(token: str):
 
   except JWTError:
     return None 
+
+def get_current_user(
+  token: Annotated[str, Depends(oauth2_scheme)],
+  db: db_dependency
+  ):
+
+  try:
+    payload = jwt.decode(
+      token,
+      JWT_SECRET_KEY,
+      algorithms=[JWT_ALGORITHM]
+    )
+
+    user_id = int(payload["sub"])
+    
+    user = db.query(User).filter(User.id == user_id).first()
+
+    if user is None:
+      raise HTTPException(
+        status_code=401,
+        detail="User is not found."
+      )
+    
+
+  except JWTError:
+    raise HTTPException(
+      status_code=401,
+      detail="Token is invalid or expired."
+    )
+  
+  return user
