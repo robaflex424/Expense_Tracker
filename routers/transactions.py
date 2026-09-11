3a63747f057bd25d19aa933d8f7e00317f849605
Roberti Pichkhnarashvili <robaflex77@gmail.com>
2026-09-11 13:49:45 +0400

    added an endpoint for getting all the transactions of specific user, and added also filtering and sorting

--- routers/transactions.py
+++ routers/transactions.py
@@ -1,4 +1,11 @@
-from fastapi import APIRouter, HTTPException, status, Depends
+from mmap import PAGESIZE
+from fastapi import (
+  APIRouter, 
+  HTTPException, 
+  status, 
+  Depends, 
+  Query)
+from sqlalchemy.orm import query
 from models.category import Category
 from models.transaction import Transaction
 from models.user import User
@@ -46,3 +53,42 @@ async def create_transaction(
   db.refresh(transaction_model)
 
   return transaction_model
+
+@router.get("", status_code=status.HTTP_200_OK, response_model=list[TransactionResponse])
+async def get_transactions(
+  db: db_dependency,
+  current_user: User = Depends(get_current_user),
+  page: int = Query(1, ge=1),
+  page_size: int = Query(10, ge=1, le=100),
+  sort_by: str = Query("created_at"),
+  sort_order: str = Query("desc")
+  ):
+  
+  if sort_by not in ["created_at", "transaction_date", "amount"]:
+    raise HTTPException(
+      status_code=status.HTTP_400_BAD_REQUEST,
+      detail="Invalid sort field."
+    )
+  
+  if sort_order not in ["asc", "desc"]:
+    raise HTTPException(
+      status_code=status.HTTP_400_BAD_REQUEST,
+      detail="Invalid sort order."
+    )
+  
+  offset = (page - 1) * page_size
+
+  query = db.query(Transaction).filter(
+    Transaction.user_id == current_user.id
+  )
+
+  sort_column = getattr(Transaction, sort_by)
+
+  if sort_order == "asc":
+    query = query.order_by(sort_column.asc())
+  else:
+    query = query.order_by(sort_column.desc())
+  
+  transactions = query.offset(offset).limit(page_size).all()
+
+  return transactions
